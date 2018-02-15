774b2203315683dd48f8869beeb2813ade01e59a
LukeLin <dev.linxu@gmail.com>
2018-02-14 22:10:32 -0500

    update show_w_h.py

--- projects/capstone/show_w_h.py
+++ projects/capstone/show_w_h.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 
@@ -10,13 +9,12 @@ def addToMap(num, map):
     else:
         map[num] = 1
 
-def showData(map):
-    print(len(width_map))
-    print(max(width_map))
-    bin = np.arange(0, 700, 50)
-    plt.hist(width_map)
-    plt.title('histograms for width')
-    plt.xlabel('width')
+def showData(map, title, xlabel):
+    print(len(map))
+    print(max(map))
+    plt.hist(map)
+    plt.title(title)
+    plt.xlabel(xlabel)
     plt.ylabel('count')
     plt.show()
 
@@ -24,11 +22,20 @@ TEST_DIR = '/Users/LukeLin/Downloads/test/'
 
 width_map = []
 height_map = []
+w = 0
+h = 0
 for file in os.listdir(TEST_DIR):
     with Image.open(TEST_DIR+file) as img:
         width, height = img.size
         width_map.append(width)
         height_map.append(height)
+        if height > h:
+            h = height
+            print('height max', height, file)
 
-showData(width_map)
-showData(height_map)
+        if width > w:
+            w = width
+            print('width max', width, file)
+
+showData(width_map, 'Test Image Width distribution', 'Width')
+showData(height_map, 'Test Image Height distribution', 'Height')
