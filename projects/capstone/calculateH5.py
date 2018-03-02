e2b4e8e29ae702efb7cc2239c03563c13ebd1a43
LukeLin <dev.linxu@gmail.com>
2018-03-02 00:48:44 -0500

    update file

--- projects/capstone/calculateH5.py
+++ projects/capstone/calculateH5.py
@@ -6,7 +6,8 @@ from keras.preprocessing.image import *
 import h5py
 
 
-def write_gap(MODEL, image_size, lambda_func=None):
+def create_h5(MODEL, image_size, lambda_func=None):
+    print(MODEL, 'start')
     width = image_size[0]
     height = image_size[1]
     input_tensor = Input((height, width, 3))
@@ -17,26 +18,21 @@ def write_gap(MODEL, image_size, lambda_func=None):
     model = Model(base_model.input, GlobalAveragePooling2D()(base_model.output))
 
     gen = ImageDataGenerator()
-    train_generator = gen.flow_from_directory("../../../input/train", image_size, shuffle=False,
+    train_generator = gen.flow_from_directory("input/train", image_size, shuffle=False,
                                               batch_size=16)
-    valid_generator = gen.flow_from_directory("../../../input/valid", image_size, shuffle=False,
-                                             batch_size=16, class_mode=None)
-    test_generator = gen.flow_from_directory("../../../input/test", image_size, shuffle=False,
+    test_generator = gen.flow_from_directory("input/test1", image_size, shuffle=False,
                                              batch_size=16, class_mode=None)
 
-    train = model.predict_generator(train_generator, train_generator.samples)
-    test = model.predict_generator(test_generator, test_generator.samples)
-    valid = model.predict_generator(valid_generator, valid_generator.samples)
-    with h5py.File("gap_%s.h5"%MODEL.func_name) as h:
+    train = model.predict_generator(train_generator, train_generator.samples/16)
+    test = model.predict_generator(test_generator, test_generator.samples/16)
+    with h5py.File("feature_%s.h5"%MODEL.__name__) as h:
         h.create_dataset("train", data=train)
-        h.create_dataset("valid", data=valid)
         h.create_dataset("test", data=test)
-        h.create_dataset("train_label", data=train_generator.classes)
-        h.create_dataset("valid_label", data=valid_generator.classes)
+        h.create_dataset("label", data=train_generator.classes)
 
 
-write_gap(ResNet50, (224, 224))
-write_gap(Xception, (299, 299), xception.preprocess_input)
-write_gap(InceptionV3, (299, 299), inception_v3.preprocess_input)
-write_gap(VGG16, (224, 224))
-write_gap(VGG19, (224, 224))
+create_h5(ResNet50, (224, 224))
+create_h5(Xception, (299, 299), xception.preprocess_input)
+create_h5(InceptionV3, (299, 299), inception_v3.preprocess_input)
+create_h5(VGG16, (224, 224))
+create_h5(VGG19, (224, 224))
